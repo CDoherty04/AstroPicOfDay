7e98b2c630b57d5349544528b3b47f594ba079d9
Charlie Doherty <charlieedoherty@gmail.com>
2024-03-08 16:36:34 -0600

    Add the date, credit, url, and title

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ st.set_page_config(layout="wide", page_title="APOD")
 
 # Write the title and credit at the top of the page
 st.markdown("<h1 style='text-align: center;'>ASTRONOMY PICTURE OF THE DAY</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center;'>Using NASA's Open API</h3>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>Using NASA's Open API</h4>", unsafe_allow_html=True)
+st.markdown(f"<p style='text-align: center;'>{content['date']}</p>", unsafe_allow_html=True)
 st.markdown("***")
 
-# Get the picture and description from NASA's Open API
+# Get the title, picture, date, and description from NASA's Open API
+st.markdown(f"<h2 style='text-align: center;'>{content['title']}</h2>", unsafe_allow_html=True)
+st.markdown(f"<a style='text-align: center;'>{content['url']}</a>", unsafe_allow_html=True)
 st.image(content["hdurl"])
 st.markdown(f"<p style='text-align: center;'>{content['explanation']}</p>", unsafe_allow_html=True)
+st.markdown(f"<p style='text-align: center;'>Credits to: {content['copyright']}</p>", unsafe_allow_html=True)
 st.markdown("***")
